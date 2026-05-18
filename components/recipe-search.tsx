06ce6794b9394d4743227c3494c97e62fd8ee549
BrentMosier <brentmosier1@gmail.com>
2026-05-18 15:07:56 -0400

    moved recipe search field to a client side component and gave it a debounce

---
+++ components/recipe-search.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { useDebounce } from "../hooks/useDebounce";
+import { useState, useEffect } from "react";
+
+export default function RecipeSearch({
+    onSearchAction,
+}: {
+    onSearchAction: (term: string) => Promise<void>;
+}) {
+    const [searchTerm, setSearchTerm] = useState("");
+    const debouncedSearchTerm = useDebounce(searchTerm, 300);
+
+    useEffect(() => {
+        console.log("useEffect triggered", debouncedSearchTerm);
+        onSearchAction(debouncedSearchTerm);
+    }, [debouncedSearchTerm, onSearchAction]);
+
+    return (
+        <Input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search recipes..."
+        />
+    );
+}
