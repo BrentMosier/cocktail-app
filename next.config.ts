ffe0169d48c5360b2da4a459d4aa5a7e63e5e600
BrentMosier <brentmosier1@gmail.com>
2026-08-04 14:21:35 -0400

    updates to host on github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    output: "export", //enables static exports for github pages hosting
+    basePath: "/various",
+    images: {
+      unoptimized: true
+    },
+    reactStrictMode: true
 };
 
 export default nextConfig;
