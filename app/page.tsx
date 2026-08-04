0108fc7aa23f81a72bacd5cfe3eda25646cc0581
BrentMosier <brentmosier1@gmail.com>
2026-08-04 14:33:18 -0400

    pointless commit to update

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
                 <p>
-                    This application is a constant work in progress. It will house a mix of various small apps I want to have hosted, but aren't big enough to spin up in their own instance.
+                    This application is a constant work in progress. It will house a mix of various small apps I want to have hosted, but are not big enough to spin up in their own instance.
                 </p>
                 <Link className="hover:underline" href="/recipes">
                     Recipes
