ea8c7593ef2f5aa0b4e8e40c220cd18ea182f7b0
BrentMosier <brentmosier1@gmail.com>
2026-08-04 14:27:55 -0400

    update base path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     output: "export", //enables static exports for github pages hosting
-    basePath: "/various",
+    // basePath: "/various",
     images: {
       unoptimized: true
     },
