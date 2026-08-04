ab8cb4b2469a91e2b362566cd649d2b928e55abe
BrentMosier <brentmosier1@gmail.com>
2026-08-04 15:50:00 -0400

    fix issue of hardcoding localhost

--- server/recipes.ts
+++ server/recipes.ts
@@ -17,9 +17,18 @@ export const fetchRecipes = async ({
     base = [],
 }: RecipeParams): Promise<Recipe[]> => {
     const formattedBase = base.join("-");
-    const response = await fetch(
-        `http://localhost:3000/api/recipes?limit=${limit}&page=${page}&query=${query}&base=${formattedBase}`,
-    );
+    // const response = await fetch(
+    //     `http://localhost:3000/api/recipes?limit=${limit}&page=${page}&query=${query}&base=${formattedBase}`,
+    // );
+    // Automatically switch between Vercel production and local host
+        const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+            ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+            : 'http://localhost:3000';
+
+        // If calling from a Client Component, a relative path `/api/recipes...` works perfectly too
+        const response = await fetch(
+            `${baseUrl}/api/recipes?limit=${limit}&page=${page}&query=${query}&base=${formattedBase}`,
+        );
     const data = await response.json();
     return data as Recipe[];
 };
@@ -38,7 +47,16 @@ export const useRecipes = ({
 
 //single recipe search
 export const fetchRecipe = async (id: string): Promise<Recipe> => {
-    const response = await fetch(`http://localhost:3000/api/recipes/${id}`);
+    // const response = await fetch(`http://localhost:3000/api/recipes/${id}`);
+    // Automatically switch between Vercel production and local host
+        const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+            ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+            : 'http://localhost:3000';
+
+        // If calling from a Client Component, a relative path `/api/recipes...` works perfectly too
+        const response = await fetch(
+            `${baseUrl}/api/recipes/${id}`,
+        );
     const data = await response.json();
     return data as Recipe;
 };
